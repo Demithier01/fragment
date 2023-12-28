c984f21dccd0e7337123fd2e7ff936529f53812c
Demithier01 <darika.ko@ku.th>
2023-12-28 22:22:43 +0700

    update

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -6,6 +6,10 @@ import android.widget.Button
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var fragmentManager: FragmentManager
@@ -14,29 +18,34 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnSecond: Button
     private lateinit var btnThird: Button
     private lateinit var fragmentCont: FrameLayout
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnFirst = findViewById(R.id.btn_first)
-        btnSecond = findViewById(R.id.btn_second)
-        btnThird = findViewById(R.id.btn_third)
-        fragmentCont = findViewById(R.id.fragmentCont)
-
-        btnFirst.setOnClickListener {
-            reSultFragment(FirstFragment())
-        }
-        btnSecond.setOnClickListener {
-            reSultFragment(SecondFragment())
-        }
-        btnThird.setOnClickListener {
-            reSultFragment(ThirdFragment())
-        }
+//
+//        btnFirst = findViewById(R.id.btn_first)
+//        btnSecond = findViewById(R.id.btn_second)
+//        btnThird = findViewById(R.id.btn_third)
+//        fragmentCont = findViewById(R.id.fragmentCont)
+//
+//        btnFirst.setOnClickListener {
+//            reSultFragment(FirstFragment())
+//        }
+//        btnSecond.setOnClickListener {
+//            reSultFragment(SecondFragment())
+//        }
+//        btnThird.setOnClickListener {
+//            reSultFragment(ThirdFragment())
+//        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentCont) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 
-    private fun reSultFragment(fragment: Fragment) {
-        fragmentManager = supportFragmentManager
-        fragmentManager.beginTransaction().replace(R.id.fragmentCont, fragment).commit()
-    }
+//    private fun reSultFragment(fragment: Fragment) {
+//        fragmentManager = supportFragmentManager
+//        fragmentManager.beginTransaction().replace(R.id.fragmentCont, fragment).commit()
+//    }
 }
