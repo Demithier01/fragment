64dea2d559f0aeb638ee358ff05cf1944c1a08fa
Demithier01 <darika.ko@ku.th>
2024-01-08 14:21:55 +0700

    update

--- app/src/main/java/com/example/fragment/SecondFragment.kt
+++ app/src/main/java/com/example/fragment/SecondFragment.kt
@@ -77,13 +77,13 @@ class SecondFragment : Fragment() {
         secondRecyclerView.layoutManager = LinearLayoutManager(requireContext())
 
         val imageUrl = mutableListOf(
-            "https://f.ptcdn.info/467/059/000/pe8fyp1u2KWzzTuJad5-o.jpg",
-            "https://www.ryoiireview.com/upload/article/201703/1490937411_1bfe8767df7c3b51ed4a759f42b3dd39.jpg",
-            "https://cdn.pixabay.com/photo/2018/12/11/09/34/bingsu-3868700_1280.jpg",
-            "https://www.ryoiireview.com/upload/article/201802/1517902751_467d516f5e5ba2f0f1a73535a33b2db2.jpg",
-            "https://img.wongnai.com/p/1920x0/2017/05/16/aa2b9594ced4452bb1e2949e9ab2ed38.jpg",
-            "https://www.ryoiireview.com/upload/article/201611/1479353464_d501901ff6df45f06a82566690bb0597.jpg",
-            "https://pbs.twimg.com/media/C4t22TaVMAA51vX.jpg"
+            "https://www.aromathailand.com/wp-content/uploads/2023/09/Americano_pic3.jpeg",
+            "https://www.aromathailand.com/wp-content/uploads/2023/04/Cover-Mocha-1080x675.jpg",
+            "https://www.nescafe.com/th/sites/default/files/2023-08/RecipeHero_IcedCaramelLatte_1066x1066.jpg",
+            "https://d1mf4ril8efyfr.cloudfront.net/media/store_557/products/d3aeb4f5-5af6-4bf3-9936-582b2b2a9394.jpg",
+            "https://madeinrecipes.com/wp-content/uploads/2021/06/%E0%B8%8A%E0%B8%B2%E0%B9%84%E0%B8%97%E0%B8%A2%E0%B8%84%E0%B8%B5%E0%B9%82%E0%B8%95-%E0%B8%8A%E0%B8%87%E0%B8%87%E0%B9%88%E0%B8%B2%E0%B8%A2-%E0%B8%AB%E0%B8%AD%E0%B8%A1%E0%B9%83%E0%B8%9A%E0%B8%8A%E0%B8%B2-Keto-Thai-Tea.jpg",
+            "https://static.vecteezy.com/system/resources/previews/016/586/251/large_2x/product-cold-drink-menu-of-mixed-cocoa-chocolate-mint-drink-in-a-plastic-glass-free-photo.jpg",
+            "https://img.freepik.com/premium-photo/iced-peach-drink-perfect-drink-catalog_718046-1398.jpg"
         )
         teaSecondAdapter = TeaSecondAdapter(imageUrl, viewPager2)
 
@@ -99,10 +99,19 @@ class SecondFragment : Fragment() {
 
     private fun addDataList() {
         secondList.add(Second("Americano", "https://www.aromathailand.com/wp-content/uploads/2023/09/Americano_pic3.jpeg"))
-        secondList.add(Second("Coco", "https://api2.krua.co/wp-content/uploads/2020/07/RD0198_Gallery_-02-scaled.jpg"))
         secondList.add(Second("Espresso", "https://www.nestleprofessional.co.th/sites/default/files/styles/np_recipe_detail/public/2022-04/nescafe-iced-espresso-540x400.jpg?itok=cGl4pcYu"))
+        secondList.add(Second("Yuzu orange coffee fresh","https://chefoldschool.com/wp-content/uploads/2023/05/3-1.jpg"))
+        secondList.add((Second("Mocha","https://www.aromathailand.com/wp-content/uploads/2023/04/Cover-Mocha-1080x675.jpg")))
+        secondList.add(Second("Latte", "https://img.salehere.co.th/p/600x0/2021/06/29/9qouf4swhzge.jpg"))
+        secondList.add(Second("Caramel Macchiato", "https://www.nescafe.com/th/sites/default/files/2023-08/RecipeHero_IcedCaramelLatte_1066x1066.jpg"))
+        secondList.add(Second("Coco", "https://api2.krua.co/wp-content/uploads/2020/07/RD0198_Gallery_-02-scaled.jpg"))
+        secondList.add(Second("Bubble Tea", "https://files.gqthailand.com/uploads/mm8.jpg"))
+        secondList.add(Second("Matcha Latte", "https://d1mf4ril8efyfr.cloudfront.net/media/store_557/products/d3aeb4f5-5af6-4bf3-9936-582b2b2a9394.jpg"))
         secondList.add(Second("GreenTea", "https://img.wongnai.com/p/1920x0/2019/02/02/1e0e327a52ce405196bb7242d22ea752.jpg"))
         secondList.add(Second("ThaiTea", "https://madeinrecipes.com/wp-content/uploads/2021/06/%E0%B8%8A%E0%B8%B2%E0%B9%84%E0%B8%97%E0%B8%A2%E0%B8%84%E0%B8%B5%E0%B9%82%E0%B8%95-%E0%B8%8A%E0%B8%87%E0%B8%87%E0%B9%88%E0%B8%B2%E0%B8%A2-%E0%B8%AB%E0%B8%AD%E0%B8%A1%E0%B9%83%E0%B8%9A%E0%B8%8A%E0%B8%B2-Keto-Thai-Tea.jpg"))
-
+        secondList.add(Second("Cocoa Mint","https://static.vecteezy.com/system/resources/previews/016/586/251/large_2x/product-cold-drink-menu-of-mixed-cocoa-chocolate-mint-drink-in-a-plastic-glass-free-photo.jpg"))
+        secondList.add(Second("Peach Tea","https://img.freepik.com/premium-photo/iced-peach-drink-perfect-drink-catalog_718046-1398.jpg"))
+        secondList.add(Second("Nestea Black Tea","https://www.nestleprofessional.co.th/sites/default/files/styles/np_recipe_detail/public/2022-04/nestea-black-tea-540x400.jpg?itok=abNXW_oE"))
+        secondList.add(Second("Lemon Tea", "https://img.th.my-best.com/content_section/beforehand_tips/885dc78edcd76c89663393d15bbee0f2.jpeg?ixlib=rails-4.3.1&q=70&lossless=0&w=690&fit=max&s=76aaeec9170346ea37132c48ceea88a9"))
     }
 }
--- app/src/main/java/com/example/fragment/third_item/BingsuFragment.kt
+++ app/src/main/java/com/example/fragment/third_item/BingsuFragment.kt
@@ -47,13 +47,14 @@ class BingsuFragment : Fragment() {
 
     }
     private fun addImageUrl(){
-        imageUrl.add(BingSu("BingSu","https://f.ptcdn.info/467/059/000/pe8fyp1u2KWzzTuJad5-o.jpg"))
-        imageUrl.add(BingSu("BingSu","https://cdn.pixabay.com/photo/2018/12/11/09/34/bingsu-3868700_1280.jpg"))
-        imageUrl.add(BingSu("BingSu","https://pbs.twimg.com/media/C4t22TaVMAA51vX.jpg"))
-        imageUrl.add(BingSu("BingSu","https://www.ryoiireview.com/upload/article/201703/1490937411_1bfe8767df7c3b51ed4a759f42b3dd39.jpg"))
-        imageUrl.add(BingSu("BingSu","https://www.ryoiireview.com/upload/article/201802/1517902751_467d516f5e5ba2f0f1a73535a33b2db2.jpg"))
-        imageUrl.add(BingSu("BingSu","https://img.wongnai.com/p/1920x0/2017/05/16/aa2b9594ced4452bb1e2949e9ab2ed38.jpg"))
-        imageUrl.add(BingSu("BingSu","https://www.ryoiireview.com/upload/article/201611/1479353464_d501901ff6df45f06a82566690bb0597.jpg"))
+        imageUrl.add(BingSu("Green Tea bingsu","https://f.ptcdn.info/467/059/000/pe8fyp1u2KWzzTuJad5-o.jpg"))
+        imageUrl.add(BingSu("Mango bingsu","https://cdn.pixabay.com/photo/2018/12/11/09/34/bingsu-3868700_1280.jpg"))
+        imageUrl.add(BingSu("Strawberry bingsu","https://pbs.twimg.com/media/C4t22TaVMAA51vX.jpg"))
+        imageUrl.add(BingSu("Melon bingsu","https://pbs.twimg.com/media/CygkiF7VQAQY9Oz.jpg:large"))
+        imageUrl.add(BingSu("Durian bingsu","https://undubzapp.com/wp-content/uploads/2019/04/Seoul-bingsu.jpg"))
+        imageUrl.add(BingSu("Thai Tea bingsu","https://img.wongnai.com/p/1920x0/2017/05/16/aa2b9594ced4452bb1e2949e9ab2ed38.jpg"))
+        imageUrl.add(BingSu("Choco Brownie bingsu","https://media-cdn.tripadvisor.com/media/photo-p/09/75/1e/8a/o-ma-spoon.jpg"))
+        imageUrl.add(BingSu("Red Bean Korea bingsu","https://image.posttoday.com/media/content/2016/05/20/F3C2F68E8DEE4328B2A5C4E89C6418D2.jpg"))
 
     }
 
